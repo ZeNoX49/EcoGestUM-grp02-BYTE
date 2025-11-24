e4e1d794fdbcd045a778f2acb54ea28870d11f9f
MUSSET Thomas <thomas.musset.etu@univ-lemans.fr>
2025-11-24 17:13:25 +0100

    Correction de 2-3 trucs

--- app/controller/eventController.php
+++ app/controller/eventController.php
@@ -5,20 +5,19 @@ require_once "app/model/eventModel.php";
 class EventController {
     public function show()
     {
-      if(!isset($_SESSION['user_role'])){
-        header('Location: /ecogestum-grp12-byte/event/show');
-      }
-      $role = $_SESSION['user_role'];
-        if ($role === 'teacher') {
-            include "app/view/eventTeacher.php";
-        } elseif ($role === 'Presidence') {
-            include "app/view/eventTeacher.php";
-        } elseif ($role === 'student') {
-            include "app/view/eventStudent.php";
-        } else {
-            header('Location: /ecogestum-grp12-byte/event/show');
+        if(!isset($_SESSION['user_role'])){
+            header('Location: index.php?action=event/show');
+        }
+
+        switch ($_SESSION['user_role']) {
+            case 'teacher':
+                include $_ENV['BONUS_PATH']."app/view/eventTeacher.php";
+            case 'Presidence':
+                include $_ENV['BONUS_PATH']."app/view/eventTeacher.php";
+            case 'student':
+                include $_ENV['BONUS_PATH']."app/view/eventStudent.php";
+            default:
+                header('Location: index.php?action=event/show');
         }
     }
 }
-
-?>
