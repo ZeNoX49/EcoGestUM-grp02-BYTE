4c27564125afc0d84309ab9bc73e21276226e9af
Nafy <brewen.chausse.fr@gmail.com>
2025-11-24 16:35:04 +0100

    commit correction

--- app/view/eventStudent.php
+++ app/view/eventStudent.php
@@ -4,15 +4,11 @@
   <meta charset="UTF-8">
   <title>EcoGestUM - Événements</title>
   <link rel="stylesheet" href="../../assets/css/style-event.css">
-<<<<<<< Updated upstream
   <link rel="stylesheet" href="../../assets/css/footerAndHeader.css">
-=======
->>>>>>> Stashed changes
   <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Koulen&family=Lexend:wght@100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Press+Start+2P&family=Roboto:ital,wght@0,100..900;1,100..900&family=Sour+Gummy:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
  </head>
   <header class="header">
-<<<<<<< Updated upstream
     <div class="bandeau">
         <ul>
             <li><img src="../../assets/image/facebook.svg" alt="Facebook"></li>
@@ -36,9 +32,6 @@
             </ul>
         </nav>
     </div>
-=======
-    <?php include 'assets/html/header.html'; ?>
->>>>>>> Stashed changes
 </header>
 <body>
 <main>
