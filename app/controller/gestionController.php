c93a87e0a4487b638eabe2a9f720f4ca6a90f6ce
Faraux Yanis <yanis.faraux@gmail.com>
2025-11-24 11:20:30 +0100

    correction session

--- app/controller/gestionController.php
+++ app/controller/gestionController.php
@@ -5,10 +5,10 @@ class gestionController
     public function show()
     {
       //Récuperer le rôle de l'utilisateur
-      if(!isset($_SESSION['role'])){
+      if(!isset($_SESSION['user_role'])){
         header('Location: /ecogestum-grp12-byte/connexion/show');
       }
-      $role = $_SESSION['role'];
+      $role = $_SESSION['user_role'];
         include "app/view/gestionView.php";
     }
 }
