f533899f6c1e801e9f50c85170fb4d596930cf7c
MUSSET Thomas <thomas.musset.etu@univ-lemans.fr>
2025-11-25 11:19:45 +0100

    Création de chaque model + correction d'un bug dans le script d'insertion.sql

--- app/controller/profilController.php
+++ app/controller/profilController.php
@@ -1,9 +1,12 @@
 <?php
 
+require_once "app/model/categorieModel.php";
+
 class profilController
 {
     public function show()
     {
+        var_dump(getAllCategories());
         include $_ENV['BONUS_PATH']."app/view/profilView.php";
     }
 }
