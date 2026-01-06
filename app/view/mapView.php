74cb9e776de80b0c21dbaeec9909bff0938f4457
Faraux Yanis <yanis.faraux@gmail.com>
2026-01-06 10:40:24 +0100

    Map d'envel

--- app/view/mapView.php
+++ app/view/mapView.php
@@ -123,7 +123,9 @@
         <?php
         $coords = [$objet['latitude'], $objet['longitude']];
         // Gestion de l'image
-        $img = getObjectImage($objet["id_objet"]);
+        $img = !empty($objet['image_objet']) ? 'assets/image/uploads/'.$objet['image_objet'] : 'assets/image/logo.svg';
+        if(strpos($objet['image_objet'], 'http') === 0) { $img = $objet['image_objet']; }
+
         // On vérifie qu'on a bien des coordonnées
         if ($coords[0] != 0 && $coords[1] != 0):
         ?>
